a667e02419f294a215f172de1e408dfa698e0433
Victor <139719863+victor0989@users.noreply.github.com>
2023-10-26 10:48:37 +0200

    Update liquid_motor.py

--- liquid_motor.py
+++ liquid_motor.py
@@ -13,3 +13,19 @@ Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 NameError: name 'oxidizer_tank' is not defined
 >>> example_liquid.add_tank(tank=fuel_tank, position=1.8)
+
+N2O_flow_tank = MassFlowRateBasedTank(
+    name="MassFlowRateBasedTank",
+    geometry=cylindrical_geometry,
+    flux_time=24.750,
+    liquid=liquid_N2O,
+    gas=vapour_N2O,
+    initial_liquid_mass=42.8,
+    initial_gas_mass=0.1,
+    liquid_mass_flow_rate_in=0,
+    liquid_mass_flow_rate_out="../data/motors/liquid_motor_example/liquid_mass_flow_out.csv",
+    gas_mass_flow_rate_in=0,
+    gas_mass_flow_rate_out="../data/motors/liquid_motor_example/gas_mass_flow_out.csv",
+    discretize=100,
+)
+
